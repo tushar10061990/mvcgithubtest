ad4122effd373456dc49162d95d80966038226a2
tushar tanwar <tushar.tanwar1990@gmail.com>
2025-10-28 23:41:21 +0530

    git hub repo

---
+++ WebApplicationgithub/WebApplicationgithub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationgithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
